51c958c04d1be299c34d5777bba2b6364772c5c7
buehlmau <buehlmau@students.zhaw.ch>
2024-05-26 12:21:19 +0200

    Änderung V1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('doch nicht fertig :-)))');
+  res.send('endlich fertig? V1');
 });
 
 app.listen(PORT, HOST);
